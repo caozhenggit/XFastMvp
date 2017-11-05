a884318f03db1d2292a19fbfddec1886ad2fded5
caozheng123 <1074401754@qq.com>
2017-11-05 13:33:25 +0800

    修改配置文件

--- XFastMvp/app/src/main/java/com/caozheng/xfastmvp/XFastConf.java
+++ XFastMvp/app/src/main/java/com/caozheng/xfastmvp/XFastConf.java
@@ -1,18 +1,18 @@
 package com.caozheng.xfastmvp;
 
 /**
- * @author 10744
+ * @author caozheng
  * @date 2017/10/28
  * <p>
  * describe:
  */
 public class XFastConf {
 
-    // #log
+    /** 是否打印log */
     public static boolean LOG = true;
-    public static String LOG_TAG = "XDroid";
+    public static String LOG_TAG = "XFastConf";
 
-    // #cache
-    public static String CACHE_SP_NAME = "config";
-    public static String CACHE_DISK_DIR = "cache";
+    /** 缓存相关文件名 */
+    public static String CACHE_SP_NAME = "cache_sp";
+    public static String CACHE_DISK_DIR = "cache_disk";
 }
